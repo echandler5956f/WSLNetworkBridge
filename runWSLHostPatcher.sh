9e4357a4ee5eb047b932793cac170c8cc0b9d7ed
Ethan Chandler <93096172+echandler5956f@users.noreply.github.com>
2022-04-28 10:07:51 -0400

    Add files via upload

---
+++ runWSLHostPatcher.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+
+while [ 1 ]
+do
+/mnt/c/Users/Ethan/WSLHostPatcher/WSLHostPatcher.exe 
+# change this to the wherever you placed the WSLHostPatcher.exe
+sleep 0.05 
+# change this to a time that is acceptable to your use case. there are better ways to do this, but it works fine enough for running ros on a turtlebot3
+done
